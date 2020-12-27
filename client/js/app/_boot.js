66c5ed22849eaa05cd5edb81dfefafa20de8ce63
hique023 <pedroviski10@hotmail.com>
2020-12-27 09:36:10 -0300

    Atividade 2

--- client/js/app/_boot.js
+++ client/js/app/_boot.js
@@ -1,7 +1,16 @@
-import { FinancaController } from './controller/FinancaController'
+import { FinancaController } from "./controller/FinancaController";
 
 let financaController = new FinancaController();
 
-document.querySelector('form').onsubmit = financaController.adiciona.bind(financaController)
-document.querySelector('#btn-import').onclick = financaController.importaFinancas.bind(financaController)
-document.querySelector('#btn-apaga').onclick = financaController.apaga.bind(financaController)
+document.querySelector("form").onsubmit = financaController.adiciona.bind(
+  financaController
+);
+document.querySelector(
+  "#btn-import"
+).onclick = financaController.importaFinancas.bind(financaController);
+document.querySelector("#btn-apaga").onclick = financaController.apaga.bind(
+  financaController
+);
+document.querySelector("#btn-filtro").onclick = financaController.filtra.bind(
+  financaController
+);
