4043f4a4717ceb3426018d431c4050930a8f0818
hique023 <pedroviski10@hotmail.com>
2020-12-27 09:43:27 -0300

    Atividade 3

--- client/js/app/models/ListaFinancas.js
+++ client/js/app/models/ListaFinancas.js
@@ -1,25 +1,31 @@
 export class ListaFinancas {
-    constructor() {
-        this._financas = []
-    }
+  constructor() {
+    this._financas = [];
+  }
 
-    adiciona(financa) {
-        this._financas.push(financa)
-    }
+  adiciona(financa) {
+    this._financas.push(financa);
+  }
 
-    esvazia() {
-        this._financas = []
-    }
+  esvazia() {
+    this._financas = [];
+  }
 
-    ordena(criterio) {
-        this._financas.sort(criterio)
-    }
+  ordena(criterio) {
+    this._financas.sort(criterio);
+  }
 
-    reverse() {
-        this._financas.reverse()
-    }
+  reverse() {
+    this._financas.reverse();
+  }
 
-    get financas() {
-        return [].concat(this._financas)
-    }
+  get financas() {
+    return [].concat(this._financas);
+  }
+
+  filtra(dataFiltrada) {
+    return this._financas.filter(financa =>
+        financa._data.setHours(0, 0, 0, 0).valueOf() === dataFiltrada.valueOf()
+    )
+  }
 }
